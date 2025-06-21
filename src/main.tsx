51b4dd8f8a6090f997a2afe6fbbf8a8859296013
sharathodepalli <sharathodepalli@gmail.com>
2025-06-21 08:57:00 -0400

    Updated adminportal

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
-// import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.tsx";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
